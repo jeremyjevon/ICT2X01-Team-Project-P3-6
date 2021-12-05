08ec432f55fbcf1810eb6212d673e9e459168886
Jeremy Chow <27985157+jeremyjevon@users.noreply.github.com>
2021-12-06 03:50:58 +0800

    Attempt v0.1 - Retrieve student attempts from DB

--- VR_dash/App.js
+++ VR_dash/App.js
@@ -14,14 +14,16 @@ import CreateStage from "./Pages/Stage/CreateStage";
 import Leaderboard from "./Pages/Leaderboard/Leaderboard";
 import Progression from "./Pages/Progression/Progression";
 import Dashboard from "./Pages/Dashboard/Dashboard";
-
+import S1Attempt from "./Pages/Attempt/S1Attempt";
+import S2Attempt from "./Pages/Attempt/S2Attempt";
+import S3Attempt from "./Pages/Attempt/S3Attempt";
+import S4Attempt from "./Pages/Attempt/S4Attempt";
 
 const Stack =  createStackNavigator();
 
 //Need to nest the tab inside a stack navigatior
 const App = () => {
   return (
-    
     <NavigationContainer>
       <StatusBar barStyle = "dark-content" hidden = {true}/>
       <Stack.Navigator initialRouteName='Login'>
@@ -34,13 +36,13 @@ const App = () => {
         <Stack.Screen name="LeaderboardScreen" component={Leaderboard} options={{headerShown:false}}/>
         <Stack.Screen name="ProgressionScreen" component={Progression} options={{headerShown:false}}/>
         <Stack.Screen name="DashboardScreen" component={Dashboard} options={{headerShown:false}}/>
-
+        <Stack.Screen name="S1AttemptScreen" component={S1Attempt} options={{headerShown:false}}/>
+        <Stack.Screen name="S2AttemptScreen" component={S2Attempt} options={{headerShown:false}}/>
+        <Stack.Screen name="S3AttemptScreen" component={S3Attempt} options={{headerShown:false}}/>
+        <Stack.Screen name="S4AttemptScreen" component={S4Attempt} options={{headerShown:false}}/>
       </Stack.Navigator>
     </NavigationContainer>
-    
   );
 }
 
-
 export default App;
-

eca9056128214890f03377f952c7c4178090b97a
syongkheng <33014950+syongkheng@users.noreply.github.com>
2021-12-06 22:56:20 +0800

    Home.js import bugfix
    Home.js was missing importing Linking.

--- VR_dash/Pages/Home.js
+++ VR_dash/Pages/Home.js
@@ -1,13 +1,9 @@
 import { useNavigation } from "@react-navigation/core";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Linking } from "react-native";
 import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
 import db, { auth } from "../firebase";
 
-/* 
-TODO: 
-2) Style this ugly ass page - LOW
-*/
 const HomeScreen = ({ route }) => {
   
   const navigation = useNavigation();
